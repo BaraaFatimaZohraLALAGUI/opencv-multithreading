3e84146028d6e8cb2d56e95da6762a9596d7e960
BaraaFatimaZohraLALAGUI <lalaguibaraa@gmail.com>
2024-09-18 13:47:42 +0000

    working on detection

--- multi_processing.py
+++ multi_processing.py
@@ -6,6 +6,8 @@ import os
 import multiprocessing
 import time
 from itertools import repeat
+from yolo_api import detect
+
 
 def get_vcap(channel):
     ip = "10.1.67.111"
@@ -43,16 +45,25 @@ def bound_white_space(frames_dict, fgMasks_buffer, bound_fr_fg, bounding_queue, 
             current_max_idx = np.argmax(ch_counts)
 
             if ch_counts[current_max_idx] > COUNT_THRESHOLD:
-                if prv_max_idx == -1 or (current_max_idx != prv_max_idx and ch_counts[current_max_idx] >= FRAME_SWITCH_THRESHOLD * ch_counts[prv_max_idx]):
+                if prv_max_idx == -1 or ch_counts[prv_max_idx] < COUNT_THRESHOLD or (current_max_idx != prv_max_idx and ch_counts[current_max_idx] >= FRAME_SWITCH_THRESHOLD * ch_counts[prv_max_idx]):
                     prv_max_idx = current_max_idx
 
                 x, y, w, h = cv.boundingRect(masks[prv_max_idx])
-                bounded_rect = frames_dict[prv_max_idx + 1][y:y + h, x:x + w]
+                bounded_rect = frames_dict[prv_max_idx + 1][y:y + h, x:x + w, :]
                 bounding_queue.put(bounded_rect)
-
-                bound_fr_fg[0] = cv.rectangle(frames_dict[prv_max_idx + 1], (x, y), (x + w, y + h), (0, 255, 0), 2)
+                
+                bound_fr_fg[0] = frames_dict[prv_max_idx + 1]            
                 bound_fr_fg[1] = cv.rectangle(cv.cvtColor(fgMasks_buffer[prv_max_idx + 1], cv.COLOR_GRAY2BGR), (x, y), (x + w, y + h), (0, 255, 0), 2)
+            # else:
+            #     bound_fr_fg[0] = None          
+            #     bound_fr_fg[1] = None
+             
 
+def detection(bounding_queue, DETECTION_THRESHOLD, RECT_COLOR):
+    while True:
+        if not bounding_queue.empty ():
+            bounded_rect = bounding_queue.get ()
+            detect(bounded_rect, model_name='YOLOv9 Tiny', detection_threshold=DETECTION_THRESHOLD, rect_color=RECT_COLOR)
 
 
 if __name__ == '__main__':
@@ -82,9 +93,7 @@ if __name__ == '__main__':
     while all(i not in frames_dict for i in range(1, NUM_CHANNELS + 1)):
         time.sleep(1)
 
-    height, width = next(iter(frames_dict.values())).shape[:2]
-    print(height, ' ', width)
-    
+    height, width = next(iter(frames_dict.values())).shape[:2]    
     display_frame = np.ones((height * NUM_ROWS + SPACE_ROW * (NUM_ROWS - 1) - CLOCK_HEIGHT * (NUM_ROWS - 1), 
                              width * NUM_CHANNELS + SPACE_COL * (NUM_CHANNELS - 1), 3), dtype=np.uint8) * 255
     SPACE2 = np.ones((height - CLOCK_HEIGHT, width, 3), dtype=np.uint8) * 255
@@ -99,6 +108,10 @@ if __name__ == '__main__':
     process_bound = multiprocessing.Process(target=bound_white_space, args=(frames_dict, fgMasks_buffer, bound_fr_fg, bounding_queue, COUNT_THRESHOLD, FRAME_SWITCH_THRESHOLD))
     processes.append(process_bound)
     process_bound.start()
+
+    process_detect = multiprocessing.Process(target=detection, args=(bounding_queue, DETECTION_THRESHOLD, RECT_COLOR))
+    processes.append(process_detect)
+    process_detect.start()
 
 
     while True:
@@ -116,15 +129,14 @@ if __name__ == '__main__':
                 display_frame[0:height, width * 3 + SPACE_COL * 3:, :] = frames_dict[4]
                 display_frame[height + SPACE_ROW:height * 2 + SPACE_ROW - CLOCK_HEIGHT, width * 3 + SPACE_COL * 3:, :] = cv.cvtColor(fgMasks_buffer[4], cv.COLOR_GRAY2BGR)
 
-                display_frame[height * 2 + SPACE_ROW * 2 - CLOCK_HEIGHT:height * 3 + SPACE_ROW * 2 - CLOCK_HEIGHT * 2, width + SPACE_COL:width * 2 + SPACE_COL, :] = bound_fr_fg[0][80:, :, :] if bound_fr_fg[0] is not None else SPACE2
-                # display_frame[height * 2 + SPACE_ROW * 2 - CLOCK_HEIGHT:height * 3 + SPACE_ROW * 2 - CLOCK_HEIGHT * 2, width * 2 + SPACE_COL * 2:width * 3 + SPACE_COL * 2, :] = cv.cvtColor(bound_fr_fg[1], cv.COLOR_GRAY2BGR) if bound_fr_fg[1] is not None else SPACE2
-                display_frame[height * 2 + SPACE_ROW * 2 - CLOCK_HEIGHT:height * 3 + SPACE_ROW * 2 - CLOCK_HEIGHT * 2, width * 2 + SPACE_COL * 2:width * 3 + SPACE_COL * 2, :] = bound_fr_fg[1] if bound_fr_fg[1] is not None else SPACE2
+                display_frame[height * 2 + SPACE_ROW * 2 - CLOCK_HEIGHT:height * 3 + SPACE_ROW * 2 - CLOCK_HEIGHT * 2, width + SPACE_COL:width * 2 + SPACE_COL, :] = SPACE2 if bound_fr_fg[0] is None else bound_fr_fg[0][80:, :, :] 
+                display_frame[height * 2 + SPACE_ROW * 2 - CLOCK_HEIGHT:height * 3 + SPACE_ROW * 2 - CLOCK_HEIGHT * 2, width * 2 + SPACE_COL * 2:width * 3 + SPACE_COL * 2, :] = SPACE2 if bound_fr_fg[1] is None else bound_fr_fg[1] 
+
 
             except KeyError as e:
                 print(f"Frame data is missing for channel: {e}")
                 continue
 
-            # Show the multi-channel video feed
             cv.imshow('Multi-Channel Video Feed', imutils.resize(display_frame, width=1550))
 
         if cv.waitKey(20) & 0xFF == ord('q'):
